c43d140754c108f59d1c0522e8144bdfbb888319
Shuan Wang <shuanwang@gmail.com>
2018-01-12 00:59:29 -0800

    remove last Promise call

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
+'use strict';
+
+// your browser needs support for:
+// fetch
+// async/await functions
+// FileReader capabilities
+
 export default class Base64Fetch {
   constructor(url) {
     this.url = url
   }
   async fetch(url = '', opts) {
     let _url = this.url || url
-    let base64data = await _fetch(url, opts)
+    try {
+      let base64data = await this._fetch(url, opts)
+    } catch (e) {
+      throw new Error(e)
+    }
+
     let trim = base64data.split(',', 2)
     if (trim && trim[1]) {
       return trim[1]
     } else {
-      return new Error('Result didnot return a base64 data object');
+      return new Error('Result did not return a base64 data object');
     }
   }
   async fetchAsData(url = '', opts) {
@@ -18,26 +30,21 @@ export default class Base64Fetch {
     return base64data;
   }
 
-  _fetch(url, opts = { mode: 'no-cors' }, keepAsData = true) {
+  async _fetch(url, opts = { mode: 'no-cors' }, keepAsData = true) {
     if (!url) {
       throw new Error('no url is provided')
     }
-    return new Promise((resolve, reject) => {
-      fetch(url, { mode: 'no-cors' }).then(function(response) {
-          if (response.ok) {
-            return response.blob()
-          }
-          throw new Error('bad response')
-        }).then(function(myBlob) {
-
-          var reader = new window.FileReader()
-          reader.readAsDataURL(myBlob)
-          reader.onloadend = function() {
-            var base64data = reader.result
-            resolve(base64data)
-          }
-
-        }).catch(reject)
-    })
+    let response = await fetch(url, { mode: 'no-cors' })
+    if (response.ok) {
+      let reader = new window.FileReader()
+      let blob = await response.blob()
+      reader.readAsDataURL(blob)
+      reader.onloadend = () => {
+        let base64data = reader.result
+        return base64data
+      }
+    } else {
+      throw new Error(response.status)
+    }
   }
 }
